8ead73fa2f11e63d405532229e74c28963417bfa
Priyanshi Singh <168511987+PriyanshiSingh05@users.noreply.github.com>
2026-04-11 01:51:11 +0530

    Time: 1 ms (77.22%) | Memory: 47 MB (14.14%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int left = 0 ;
+        int right = 1;
+        int ans = 1;
+
+        while(right < n){
+            if(nums[right] != nums[right -1]){
+                nums[left+1] = nums[right];
+                left++;
+                ans += 1;
+                right++;
+            }
+            else{
+                right++;
+            }
+        }return ans;
+        
+    }
+}
